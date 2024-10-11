164f2adb64c1f49853a5ecb0e3863f8d8f7b107c
Your Name <you@example.com>
2024-10-11 09:33:44 +0200

    Class Transfer

---
+++ adBank/Transfer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adBank
+{
+    internal class Transfer
+    {
+        private int id {  get; set; }
+        private Account source {  get; set; }
+        private Account target { get; set; }
+        private int amount { get; set; }
+        private string title { get; set; }
+        private DateTime timestamp { get; set; }
+    }
+}
